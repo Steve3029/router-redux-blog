e3e14e6bfe7c63915fa7c60475a9c4915b9076a6
Steve <steve.liu.3029@gmail.com>
2018-05-16 22:56:24 +1200

    added delete post feature and clean redux state function

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 export const FETCH_POSTS = 'FETCH_POSTS'
 export const CREATE_POST = 'CREATE_POST'
 export const FETCH_SIGNGLE_POST = 'FETCH_SINGLE_POST'
+export const DELETE_POST = 'DELETE_POST'
 
 const base_url = 'http://reduxblog.herokuapp.com/api'
 const api_key = 'octopus1001'
@@ -40,5 +41,19 @@ export function fetchSignlePost(id) {
   return {
     type: FETCH_SIGNGLE_POST,
     payload: request
+  }
+}
+
+export function deletePost(id, callback) {
+  const request = axios.delete(`${base_url}/posts/${id}`, {
+    params: {
+      key: api_key
+    }
+  })
+  .then(() => callback())
+
+  return {
+    type: DELETE_POST,
+    payload: id
   }
 }
--- src/containers/PostShow/index.js
+++ src/containers/PostShow/index.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import { fetchSignlePost } from '../../actions'
+import { fetchSignlePost, deletePost } from '../../actions'
 import { connect } from 'react-redux'
 
 class PostShow extends Component {
   componentDidMount() {
     const { id } = this.props.match.params
     this.props.fetchSignlePost(id)
+  }
+
+  onDeleteClick() {
+    const { id } = this.props.match.params
+    this.props.deletePost(id, () => {
+      this.props.history.push('/')
+    })
   }
 
   render () {
@@ -17,6 +24,12 @@ class PostShow extends Component {
     return (
       <div>
         <Link to="/">Back to Index</Link>
+        <button 
+          className="btn btn-danger float-right" 
+          onClick={this.onDeleteClick.bind(this)}
+        >
+          Delete Post
+        </button>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
@@ -29,4 +42,4 @@ function mapStateToProps({posts}, ownProps) {
   return { post: posts[ownProps.match.params.id] }
 }
 
-export default connect(mapStateToProps, { fetchSignlePost })(PostShow)
+export default connect(mapStateToProps, { fetchSignlePost, deletePost })(PostShow)
