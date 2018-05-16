4163a2e0860fdbca2537a2e042a19cd5f96e811b
Steve <steve.liu.3029@gmail.com>
2018-05-16 14:14:37 +1200

    connected redux and PostShow componenet

--- src/containers/PostShow/index.js
+++ src/containers/PostShow/index.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react'
+import { fetchSignlePost } from '../../actions'
+import { connect } from 'react-redux'
 
 class PostShow extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params
+    this.props.fetchSignlePost(id)
+  }
+
   render () {
+    const { post } = this.props
+    if (!post) {
+      return <div>Loading...</div>
+    }
     return (
       <div>
-        Showing Single Post.
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     )
   }
 } 
 
-export default PostShow
+function mapStateToProps({posts}, ownProps) {
+  return { post: posts[ownProps.match.params.id] }
+}
+
+export default connect(mapStateToProps, { fetchSignlePost })(PostShow)
